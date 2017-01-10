2c39dfcc4f19041f02acd8cfbb86ecbbcd41d075
Mick <mick.vanhulst@gmail.com>
2017-01-10 14:52:32 +0100

    init

--- ltest.py
+++ ltest.py
@@ -3,15 +3,14 @@ from World import World
 import threading
 import time
 
-def init_vars(game):
+def init_vars(game_world):
     discount = 0.3
-    actions = game.ACTIONS
+    actions = game_world.ACTIONS
     states = []
     Q = {}
 
-    # append all 
-    for i in range(game.AXIS_X):
-        for j in range(game.AXIS_Y):
+    for i in range(game_world.AXIS_X):
+        for j in range(game_world.AXIS_Y):
             states.append((i, j))
 
     for state in states:
@@ -20,22 +19,22 @@ def init_vars(game):
             temp[action] = 0.1
         Q[state] = temp
 
-    for (i, j, c, w) in game.SPECIALS:
+    for (i, j, c, w) in game_world.SPECIALS:
         for action in [item[0] for item in actions]:
             Q[(i, j)][action] = w
 
-    return discount, actions, Q
+    return discount, actions, states, Q
 
-def do_action(action, game, actions):
-    s = game.Player
-    r = -game.score
+def do_action(action, game_world, actions):
+    s = game_world.Player
+    r = -game_world.score
     coords = [i for i in actions if i[0] == action]
 
     AXIS_X, AXIS_Y = coords[0][1:]
-    game.try_move(AXIS_X, AXIS_Y)
+    game_world.try_move(AXIS_X, AXIS_Y)
 
-    s2 = game.Player
-    r += game.score
+    s2 = game_world.Player
+    r += game_world.score
     return s, action, r, s2
 
 
@@ -50,23 +49,26 @@ def inc_Q(s, a, alpha, inc, Q):
     Q[s][a] *= 1 - alpha
     Q[s][a] += alpha * inc
 
-def render_game(game):
-    game.render_grid()
-    game.board.grid(row=0, column=0)
+def render_game(game_world):
+    game_world.render_grid()
+    game_world.board.grid(row=0, column=0)
 
-    game.me = game.board.create_rectangle(game.Player[0]*game.WIDTH+game.WIDTH*2/10, game.Player[1]*game.WIDTH+game.WIDTH*2/10,
-        game.Player[0]*game.WIDTH+game.WIDTH*8/10, game.Player[1]*game.WIDTH+game.WIDTH*8/10, fill="orange", width=1, tag="me")
+    game_world.me = game_world.board.create_rectangle(game_world.Player[0]*game_world.WIDTH+game_world.WIDTH*2/10, game_world.Player[1]*game_world.WIDTH+game_world.WIDTH*2/10,
+        game_world.Player[0]*game_world.WIDTH+game_world.WIDTH*8/10, game_world.Player[1]*game_world.WIDTH+game_world.WIDTH*8/10, fill="orange", width=1, tag="me")
 
 
-def run(discount, game, Q, actions):
+def run(discount, game_world, Q, actions):
     time.sleep(1)
     alpha = 1
     t = 1
-    while True:       
+    while True:
+        # render grid
+        render_game(game_world)
+        
         # Pick the right action
-        s = game.Player
+        s = game_world.Player
         max_act, max_val = max_Q(s, Q)
-        (s, a, r, s2) = do_action(max_act, game, actions)
+        (s, a, r, s2) = do_action(max_act, game_world, actions)
 
         # Update Q
         max_act, max_val = max_Q(s2, Q)
@@ -74,8 +76,8 @@ def run(discount, game, Q, actions):
 
         # Check if the game has restarted
         t += 1.0
-        if game.has_restarted():
-            game.restart_game()
+        if game_world.has_restarted():
+            game_world.restart_game()
             time.sleep(0.01)
             t = 1.0
 
@@ -85,21 +87,18 @@ def run(discount, game, Q, actions):
         # MODIFAXIS_y THIS SLEEP IF THE GAME IS GOING TOO FAST.
         time.sleep(0.1)
 
-        # render game
-        render_game(game)
-
 def main(): 
-    # Init game -- input: width, x_axis & y_axis
-    game = World(100, 10, 10)
+    # Init game_world
+    game_world = World(100, 5, 5)
     
     # Init vars
-    discount, actions, Q = init_vars(game)
+    discount, actions, states, Q = init_vars(game_world)
     
     # Start game
-    t = threading.Thread(target=run, args=(discount, game, Q, actions))
+    t = threading.Thread(target=run, args=(discount, game_world, Q, actions))
     t.daemon = True
     t.start()
-    game.start_game()
+    game_world.start_game()
     
 
 if __name__ == '__main__':
